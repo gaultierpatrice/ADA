015ed09f8f49c7c1eed11ab914465b4fa29bbd93
Gaultier Patrice <gaultiercode@gmail.com>
2025-10-08 14:24:45 +0200

    exo 2 boucle fini

---
+++ exo_maison/8_Boucles/exercice2/script.js
@@ -0,0 +1,18 @@
+let letters = ["A", "d", "a", "-", "T", "e", "c", "h"]
+let message = ""
+
+for (let i = 0; i < letters.length; i++) {
+  message += letters[i]
+}
+
+console.log(message)
+
+message = ""
+
+letters = ["A", "d", "a", "-", "S", "c", "h", "o", "o", "l"]
+
+for (let i = 0; i < letters.length; i++) {
+  message += letters[i]
+}
+
+console.log(message)
