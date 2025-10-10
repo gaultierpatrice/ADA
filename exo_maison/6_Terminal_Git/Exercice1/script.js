9f71b1a6c7a81ed522eb0095619628f6e834227b
Gaultier Patrice <gaultiercode@gmail.com>
2025-10-10 16:15:27 +0200

    nouvel exo individuel

---
+++ exo_maison/6_Terminal_Git/Exercice1/script.js
@@ -0,0 +1,14 @@
+// Nous souhaitons faire la somme du prix de plusieurs articles
+// Prix de deux articles
+const article1 = 10
+const article2 = 20
+// Nous calculons le total
+let total = article1 + article2
+// Prix d'un troisieme article
+const article3 = 50
+// Nous ajoutons a3 au total
+total = total + article3
+//Nous appliquons 20% de reduction
+let total2 = total * 0.8
+console.log("Le prix total avant reduction est de : ", total)
+console.log("Le prix total après reduction est de : ", total2)
